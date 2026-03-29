dee0b0bcfced08fa886b887fe1168fc645d162b8
Priyadharshini Suresh <priyadharshinivijaya0906@gmail.com>
2026-03-29 09:27:46 +0530

    Refactor app.py to improve readability and structure
    Signed-off-by: Priyadharshini Suresh  <priyadharshinivijaya0906@gmail.com>

--- app.py
+++ app.py
@@ -21,23 +21,18 @@ def register():
         conn.close()
 
         return redirect('/login')
-
     return render_template("register.html")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-
     if request.method == 'POST':
-
         username = request.form['username']
         password = request.form['password']
 
         conn = sqlite3.connect("users.db")
         cur = conn.cursor()
-
         cur.execute("SELECT * FROM users WHERE username=? AND password=?",(username,password))
         user = cur.fetchone()
-
         conn.close()
 
         if user:
@@ -54,17 +49,14 @@ def dashboard():
 @app.route('/aptitude')
 def aptitude():
     return render_template("aptitude.html")
+
 @app.route('/submit_test', methods=['POST'])
 def submit_test():
-
     score = 0
-
     if request.form['q1'] == "a":
         score += 1
-
     if request.form['q2'] == "b":
         score += 1
-
     if request.form['q3'] == "c":
         score += 1
 
@@ -79,50 +71,39 @@ def submit_test():
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
-
     skills = request.form['skills'].lower()
 
     if "business" in skills or "management" in skills:
         career = "Business Manager"
         course = "BBA / MBA"
         college = "Example: BBA is available in MGJ College, Chennai"
-    
     elif "coding" in skills or "programming" in skills:
         career = "Software Developer"
         course = "B.Tech / B.Sc Computer Science"
         college = "Example: B.Sc CS available in Sree Muthukumaraswamy College, Chennai"
-
     elif "maths" in skills:
         career = "Data Analyst"
         course = "B.Sc Data Science"
         college = "Example: Data Science available in Loyola College, Chennai"
-
     elif "design" in skills:
         career = "Graphic Designer"
         course = "B.Des"
         college = "Example: Design courses in NIFT Chennai"
-
     else:
         career = "Career Not Found"
         course = "Explore more"
         college = "Check nearby colleges"
 
-    return render_template("courses.html",
-                           career=career,
-                           course=course,
-                           college=college)
+    return render_template("courses.html", career=career, course=course, college=college)
+
 @app.route('/admin')
 def admin():
-
     conn = sqlite3.connect("users.db")
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM users")
     users = cur.fetchall()
-
     conn.close()
-
-    return render_template("admin.html",users=users)
+    return render_template("admin.html", users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
